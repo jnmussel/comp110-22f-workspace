bf08605df4f4302320f2f4befe097ff8f21bbd8b
John Musselman <jmusselman@unc.edu>
2022-09-05 16:43:44 -0400

    resubmit even tho its wrong

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,4 +1,4 @@
-"""EX02 One Shot Wordle"""
+"""EX02 One Shot Wordle."""
 
 __author__ = "730471672"
 secret: str = str("python")
@@ -11,95 +11,16 @@ if guess == secret:
 else:
     outcome: str = "Not quite. Play again soon!"
 
-if guess[0] == str("p"):
-    first: str = str("\U0001F7E9")
-if guess[1] == str("p"):
-    first: str = str("\U0001F7E8")
-if guess[2] == str("p"):
-    first: str = str("\U0001F7E8")
-if guess[3] == str("p"):
-    first: str = str("\U0001F7E8")
-if guess[4] == str("p"):
-    first: str = str("\U0001F7E8")
-if guess[5] == str("p"):
-    first: str = str("\U0001F7E8")
-else:
-    first: str = str("\U00002B1C")
+i: int = 0
+color: str = ""
 
-if guess[1] == str("y"):
-    second: str = str("\U0001F7E9")
-if guess[0] == str("y"):
-    second: str = str("\U0001F7E8")
-if guess[2] == str("y"):
-    second: str = str("\U0001F7E8")
-if guess[3] == str("y"):
-    second: str = str("\U0001F7E8")
-if guess[4] == str("y"):
-    second: str = str("\U0001F7E8")
-if guess[5] == str("y"):
-    second: str = str("\U0001F7E8")
-else:
-    second: str = str("\U00002B1C")
+while i < len(guess):
+    if guess[i] == secret[i]:
+        color = color + str("\U0001F7E9")
+    else:
+        color = color + str("\U00002B1C")
+    i = i + 1
 
-if guess[2] == str("t"):
-    third: str = str("\U0001F7E9")
-if guess[0] == str("t"):
-    third: str = str("\U0001F7E8")
-if guess[1] == str("t"):
-    third: str = str("\U0001F7E8")
-if guess[3] == str("t"):
-    third: str = str("\U0001F7E8")
-if guess[4] == str("t"):
-    third: str = str("\U0001F7E8")
-if guess[5] == str("t"):
-    third: str = str("\U0001F7E8")
-else:
-    third: str = str("\U00002B1C")
-
-if guess[3] == str("h"):
-    fourth: str = str("\U0001F7E9")
-if guess[1] == str("h"):
-    fourth: str = str("\U0001F7E8")
-if guess[2] == str("h"):
-    fourth: str = str("\U0001F7E8")
-if guess[0] == str("h"):
-    fourth: str = str("\U0001F7E8")
-if guess[4] == str("h"):
-    fourth: str = str("\U0001F7E8")
-if guess[5] == str("h"):
-    fourth: str = str("\U0001F7E8")
-else:
-    fourth: str = str("\U00002B1C")
-
-if guess[4] == str("o"):
-    fiftht: str = str("\U0001F7E9")
-if guess[1] == str("o"):
-    fifth: str = str("\U0001F7E8")
-if guess[2] == str("o"):
-    fifth: str = str("\U0001F7E8")
-if guess[3] == str("o"):
-    fifth: str = str("\U0001F7E8")
-if guess[0] == str("o"):
-    fifth: str = str("\U0001F7E8")
-if guess[5] == str("o"):
-    fifth: str = str("\U0001F7E8")
-else:
-    fifth: str = str("\U00002B1C")
-
-if guess[5] == str("n"):
-    sixth: str = str("\U0001F7E9")
-if guess[1] == str("n"):
-    sixth: str = str("\U0001F7E8")
-if guess[2] == str("n"):
-    sixth: str = str("\U0001F7E8")
-if guess[3] == str("n"):
-    sixth: str = str("\U0001F7E8")
-if guess[4] == str("n"):
-    sixth: str = str("\U0001F7E8")
-if guess[0] == str("n"):
-    sixth: str = str("\U0001F7E8")
-else:
-    sixth: str = str("\U00002B1C")
-
-print(first + second + third + fourth + fifth + sixth)
+print(color)
 print(outcome)
+    
