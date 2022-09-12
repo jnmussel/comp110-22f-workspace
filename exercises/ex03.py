835e30eb550a501d233b850f517ebaf26e50e78d
John Musselman <jmusselman@unc.edu>
2022-09-11 22:02:11 -0400

    test2

--- exercises/ex03.py
+++ exercises/ex03.py
@@ -13,7 +13,55 @@ def contains_char(word: str, letter: str) -> bool:
         i = i + 1
     return check
 
-    
+def emojified(guess: str, secret: str) -> str:
+    """Function for colored boxes. """
+    assert len(guess) == len(secret)
+    j: int = 0
+    color: str = ""
+    while j < len(guess):
+        if guess[j] == secret[j]:
+            color = color + str("\U0001F7E9")
+        else:
+            alt: bool = False
+            k: int = 0
+            while alt is not True and k < len(secret):
+                if secret [k] == guess [j]:
+                    alt: bool = True
+                else:
+                    k = k + 1
+            if alt is True:
+                color = color + str("\U0001F7E8")
+            else:
+                color = color + str("\U00002B1C")
+        j = j + 1
+    return color
 
-        
+def input_guess(length: int) -> str:
+    """Makes sure user inputs word with right amount of letters. """
+    hey: str = input("Enter a " + str(length) +  " character word: ")
+    while len(hey) != length:
+        hey: str = input("That wasn't " + str(length) + " chars! Try again : ")
+    return hey
+    
+def main() -> None:
+    """The entrypoint of the program and main game loop."""
+    turn: int = 1
+    playing: bool = True
+    while turn <= 6 and playing is True:
+        print("=== Turn " + str(turn) + "/6 ===")
+        gameguess: str = input_guess(5)
+        gsecret: str = "codes"
+        if gameguess == gsecret:
+            print(emojified(gameguess, gsecret))
+            print("You won in " + str(turn) + "/6 turns!")
+            playing = False
+            exit()
+        else:
+            print(emojified(gameguess, gsecret))
+        turn = turn + 1
+    if turn == 6 and playing is True:
+        print("X/6 - Sorry, try again tomorrow!")
+
+exit()
+
 
