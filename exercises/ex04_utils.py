0b90dc71085f5f75422bd86b8af4a7aeaf8cf50a
John Musselman <jmusselman@unc.edu>
2022-09-12 17:17:42 -0400

    ..

---
+++ exercises/ex04_utils.py
@@ -0,0 +1,7 @@
+"""First list exercise for COMP110. """
+
+__author__ = "730471672"
+
+def all(a: list[int], b: int) -> bool:
+    """all should return a bool indicating whether or not all the ints in the list are the same as the given int."""
+    
