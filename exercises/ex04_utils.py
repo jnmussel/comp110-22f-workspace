490339ee2d99b5d9bdb0f57e529abd34f7d9f848
John Musselman <jmusselman@unc.edu>
2022-09-19 15:52:03 -0400

    .

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -7,6 +7,8 @@ def all(list1: list[int], int1: int) -> bool:
     """all should return a bool indicating whether or not all the ints in the list are the same as the given int."""
     i: int = 0
     var1: bool = True
+    if len(list1) == 0:
+        var1: bool = False
     while i < len(list1) and var1 is True:
         if list1[i] != int1:
             var1 = False
@@ -21,23 +23,25 @@ def max(input: list[int]) -> int:
         raise ValueError("max() arg is an empty List")
     k: int = 0
     var2: int = input[0]
-    while k < len(input):
+    while k < len(input) - 1:
         if input[k + 1] > input[k]:
             var2 = input[k + 1]
     k = k + 1
     return var2
     
-    
-def is_equal(list2: list[int], int2: list[int]) -> bool:
+
+def is_equal(list2: list[int], list3: list[int]) -> bool:
     """Checks for identical indices."""
-    if len(list2) > len(int2):
-        var3: int = len(int2)
+    if len(list2) > len(list3):
+        var3: int = len(list3) - 1
+    elif len(list2) == len(list3):
+        var3: int = len(list2) - 1
     else:
-        var3: int = len(list2)
+        var3: int = len(list2) - 1
     j: int = 0
     var4: bool = True
     while j < var3:
-        if list2[j] != int2[j]:
+        if list2[j] != list3[j]:
             var4 = False
     j = j + 1
     return var4
