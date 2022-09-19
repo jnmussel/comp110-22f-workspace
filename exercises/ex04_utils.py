023b1fbc78b892ec325da458c5b3882bee982233
John Musselman <jmusselman@unc.edu>
2022-09-19 15:24:37 -0400

    .

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,17 +1,19 @@
-"""First list exercise for COMP110. """
+"""First list exercise for COMP110."""
 
 __author__ = "730471672"
 
-def all(a: list[int], b: int) -> bool:
+
+def all(list1: list[int], int1: int) -> bool:
     """all should return a bool indicating whether or not all the ints in the list are the same as the given int."""
     i: int = 0
     var1: bool = True
-    while i < len(a) and var1 is True:
-        if a[i] != b:
+    while i < len(list1) and var1 is True:
+        if list1[i] != int1:
             var1 = False
         else:
             i = i + 1
     return var1
+
 
 def max(input: list[int]) -> int:
     """Finds maximum value of a list."""
@@ -25,16 +27,17 @@ def max(input: list[int]) -> int:
     k = k + 1
     return var2
     
-def is_equal(c: list[int], d: list[int]) -> bool:
+    
+def is_equal(list2: list[int], int2: list[int]) -> bool:
     """Checks for identical indices."""
-    if len(c) > len(d):
-        var3: int = len(d)
+    if len(list2) > len(int2):
+        var3: int = len(int2)
     else:
-        var3: int = len(c)
+        var3: int = len(list2)
     j: int = 0
     var4: bool = True
     while j < var3:
-        if c[j] != d[j]:
+        if list2[j] != int2[j]:
             var4 = False
     j = j + 1
     return var4
