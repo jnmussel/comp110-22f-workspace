d8f894e09075a796629f41c88f6828006a866b31
John Musselman <jmusselman@unc.edu>
2022-10-05 18:59:10 -0400

    .

--- exercises/justincase.py
+++ exercises/justincase.py
@@ -14,15 +14,14 @@ def greet() -> None:
     global player
     player = input("Welcome to the coin flipping game! What is your name? ")
     print(f"Welcome {player}! \n This is a game in which you will try to guess if a coin lands on heads or tails. \n The objective is to guess as many right as possible in a row! \n For each guessed correctly, a point will be added to your score. ")
-    print("To guess heads, enter 'H' to guess tails, enter 'T', to quit the game, enter 'Q'. ")
+    print("To guess heads, enter 'H' to guess tails, enter 'T', to quit the game, enter 'Q', and to begin a new game, enter 'N'. ")
     print("Let's start! ")
 
 
-def coin_points(player_points: int) -> int:
+def coin_points(player_points: int, counter: int) -> int:
     """Adds points to the global score."""
-    global points
-    points += player_points
-    return points
+    counter += player_points
+    return counter 
 
 
 def coin_flip_correct(player_int: int) -> bool:
@@ -37,6 +36,7 @@ def coin_flip_correct(player_int: int) -> bool:
 
 def main() -> None:
     """Main function call and game loop."""
+    points: int = 0 
 
     greet()
     
@@ -44,25 +44,29 @@ def main() -> None:
     while playing:
         playing_input: str = input(f"Flipping coin! Go ahead and call it! \n {MONEY_BAG} \n {NAMED_CONSTANT} \n {NAMED_CONSTANT} \n {NAMED_CONSTANT} \n")
 
-        if playing_input == "H" and coin_flip_correct(0) is True:
-            print("You guessed correctly! ")
-            print(f"Total points: {coin_points(2)}")
+        if playing_input == "N":
+            main()
+            playing = False 
 
-        elif playing_input == "T" and coin_flip_correct(1) is True:
+        elif playing_input == "H" and coin_flip_correct(0):
             print("You guessed correctly! ")
-            print(f"Total points: {coin_points(2)}")
+            print(f"Total points: {coin_points(2, points)}")
+
+        elif playing_input == "T" and coin_flip_correct(1):
+            print("You guessed correctly! ")
+            print(f"Total points: {coin_points(2, points)}")
         
-        elif playing_input == "Q":
+        if playing_input == "Q":
             print("Thanks for playing!")
-            print(f"Total points : {coin_points(0)}")
+            print(f"Total points : {coin_points(1, points)}")
             exit()
         
-        elif playing_input != "T" and playing_input != "H" and playing_input != "Q": 
+        if playing_input != "T" and playing_input != "H" and playing_input != "N" and playing_input != "Q": 
             print("Not valid input.")
 
         else:
             print("Sorry! You guessed wrong. Game over.")
-            print(f"Total points: {coin_points(1)}")
+            print(f"Total points: {coin_points(1, points)}")
             playing = False
 
         
